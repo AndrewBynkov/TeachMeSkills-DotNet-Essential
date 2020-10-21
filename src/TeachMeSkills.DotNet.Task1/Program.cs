f05241864096c7ec31e5ba90dc81166695c50eef
Andrew <andrew_bynkov@mail.ru>
2020-10-21 13:02:52 +0300

    feat: create new project and compleated task1

--- src/TeachMeSkills.DotNet.Task1/Program.cs
+++ src/TeachMeSkills.DotNet.Task1/Program.cs
@@ -6,9 +6,29 @@ namespace TeachMeSkills.DotNet.Task1
     {
         static void Main(string[] args)
         {
-            // Your code here..
+            double side1;
+            bool canParse;
+            do
+            {
+                Console.Write("Enter side1: ");
+                canParse = double.TryParse(Console.ReadLine(), out double val1);
+                side1 = val1;
+            }
+            while (!canParse);
 
-            Console.ReadLine();
+            double side2;
+            do
+            {
+                Console.Write("Enter side2: ");
+                canParse = double.TryParse(Console.ReadLine(), out double val1);
+                side2 = val1;
+            }
+            while (!canParse);
+
+            var rectangle = new Rectangle(side1, side2);
+            rectangle.GetValue();
+            Console.WriteLine($"Area figure - {rectangle.Area}");
+            Console.WriteLine($"Perimetr figure - {rectangle.Perimeter}");
         }
     }
 }
---
+++ src/TeachMeSkills.DotNet.Task1/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task1
+{
+    class Rectangle
+    {
+        public Rectangle(double side1, double side2)
+        {
+            _side1 = side1;
+            _side2 = side2;
+        }
+
+        /// <summary>
+        /// Area figure
+        /// </summary>
+        public double Area { get; set; }
+
+        /// <summary>
+        /// Perimetr figure
+        /// </summary>
+        public double Perimeter { get; set; }
+
+        /// <summary>
+        /// figure side1
+        /// </summary>
+        double _side1;
+
+        /// <summary>
+        /// figure side2
+        /// </summary>
+        double _side2;
+
+        public double AreaCalculator() => _side1 * _side2;
+
+        double PerimeterCalculator() => (_side1 * 2) + (_side2 * 2);
+
+        public void GetValue()
+        {
+            Area = AreaCalculator();
+            Perimeter = PerimeterCalculator();
+        }
+    }
+}
