756c1b9d8bd289cd6c67e99f5ce64c7884d881c2
Andrew <andrew_bynkov@mail.ru>
2020-11-10 01:37:26 +0300

    feat: create new project and compleated task4

---
+++ src/TeachMeSkills.DotNet.Task4/Address.cs
@@ -0,0 +1,41 @@
+﻿namespace TeachMeSkills.DotNet.Task4
+{
+    public class Address
+    {
+        public Address(int index, string country, string city, string street, string house, string apartment)
+        {
+            _index = index;
+            _country = country;
+            _city = city;
+            _street = street;
+            _house = house;
+            _apartment = apartment;
+
+            AddToProperties();
+        }
+
+        private int _index;
+        private string _country;
+        private string _city;
+        private string _street;
+        private string _house;
+        private string _apartment;
+
+        public int Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+
+        private void AddToProperties()
+        {
+            Index = _index;
+            Country = _country;
+            City = _city;
+            Street = _street;
+            House = _house;
+            Apartment = _apartment;
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var addres = new Address(220228, "Belarus", "Minsk", "Kosmonavtov", "6", "74");
+            Console.WriteLine($"Index - {addres.Index}\n" +
+                $"Country - {addres.Country}\n" +
+                $"City - {addres.City}\n" +
+                $"Street - {addres.Street}\n" +
+                $"House - {addres.House}\n" +
+                $"Appartment - {addres.Apartment}");
+        }
+    }
+}
