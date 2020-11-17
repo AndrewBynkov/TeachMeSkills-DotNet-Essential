c1038d12f9d06d601544e3eb924fe712cbe9001f
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2020-11-17 14:47:06 +0300

    feat: create new project and compleated task5

---
+++ src/TeachMeSkills.DotNet.Task5/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task5
+{
+    class Converter
+    {
+        public Converter(double usd, double eur, double rub)
+        {
+            _usd = usd;
+            _rub = rub;
+            _eur = eur;
+        }
+
+        private double _usd;
+        private double _eur;
+        private double _rub;
+
+        public double SumOfCurrency { get; set; }
+
+        public void CurrencyConvert()
+        {
+            Console.WriteLine($"BYN to USD: {SumOfCurrency / _usd}");
+            Console.WriteLine($"BYN to EUR: {SumOfCurrency / _eur}");
+            Console.WriteLine($"BYN to RUB: {SumOfCurrency / _rub}");
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter sum of currency BYN: ");
+            double sumOfCurrency = default;
+
+            bool canParce;
+            do
+            {
+                canParce =  double.TryParse(Console.ReadLine(), out double val1);
+                sumOfCurrency = val1;
+            }
+            while (!canParce);
+
+            var Converter = new Converter(2.561, 3.038, 0.0334)
+            {
+                SumOfCurrency = sumOfCurrency
+            };
+
+            Converter.CurrencyConvert();
+        }
+    }
+}
