2e0272aff6470fae908d061ef6b32e7ddb7ae3cd
Andrew <andrew_bynkov@mail.ru>
2020-12-20 14:53:41 +0300

    feat: create new project, and compleated task 7

---
+++ src/TeachMeSkills.DotNet.Task7/Invoice.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task7
+{
+    public class Invoice
+    {
+        public Invoice(int account, string customer, string provider, string article)
+        {
+            _account = account;
+            _customer = customer;
+            _provider = provider;
+            _article = article;
+        }
+
+
+        private readonly int _account;
+
+        private readonly string _customer;
+
+        private readonly string _provider;
+
+        private readonly string _article;
+
+        private int _quantity;
+
+        private decimal _cost;
+
+
+        internal (decimal, decimal) GetOrderCost(int quantity, decimal cost)
+        {
+            _quantity = quantity;
+            _cost = cost;
+
+            var orderValueWithVAT = _quantity * _cost * 1.2M;
+            var orderValueWithoutVAT = _quantity * _cost;
+
+            return (orderValueWithVAT, orderValueWithoutVAT);
+        }
+
+        internal void GetInfo(decimal withVAT, decimal withoutVAT)
+        {
+            Console.WriteLine($"Account - {_account}\n" +
+                $"Customer - {_customer}\n" +
+                $"Provider - {_provider}\n" +
+                $"Article - {_article}\n" +
+                $"Quantity - {_quantity}\n" +
+                $"Cost - {_cost}");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\nOrder Value With VAT - {withVAT}\n" +
+                $"Order Value Without VAT - {withoutVAT}");
+            Console.ResetColor();
+
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var invoice = new Invoice(193342360,"LLC AMAZON", "LLC AlphaStyle", "Computer");
+
+            (decimal withVAT, decimal withoutVAT) = invoice.GetOrderCost(2, 115);
+            invoice.GetInfo(withVAT, withoutVAT);
+        }
+    }
+}
