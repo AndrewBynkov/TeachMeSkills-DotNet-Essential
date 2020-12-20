fb290ceb368a9c1ff6e75b40d88647b02ca008fb
Andrew <andrew_bynkov@mail.ru>
2020-12-20 15:28:21 +0300

    feat: create new project and compleat task 8

---
+++ src/TeachMeSkills.DotNet.Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateNow = DateTime.Now;
+
+            var user = new User(dateNow)
+            {
+                Login = "AndyDavis",
+                FirstName = "Andrew",
+                SecondName = "Bynkov",
+            };
+
+            user.GetInfo();
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task8/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task8
+{
+    public class User
+    {
+        public User(DateTime dateNow)
+        {
+            _date = dateNow;
+        }
+
+        public string Login { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public readonly DateTime _date;
+
+
+        public void GetInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("User Info");
+            Console.ResetColor();
+            Console.WriteLine($"login - {Login}");
+            Console.WriteLine($"FirstName - {FirstName}");
+            Console.WriteLine($"SecondName - {SecondName}");
+            Console.WriteLine($"Date - {_date}");
+        }
+    }
+}
