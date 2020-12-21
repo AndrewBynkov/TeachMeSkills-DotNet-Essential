ed07038bde6c368ec010738148ec49a4a97bf51b
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2020-12-21 16:43:42 +0300

    feat: create new project and compleated Task 12

---
+++ src/TeachMeSkills.DotNet.Task12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printer = new Printer();
+            Printer test1 = new TestColor1();
+            Printer test2 = new TestColor2();
+
+            printer.Print("Andrew");
+            test1.Print("Andrew");
+            test2.Print("Andrew");
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task12/TestColor1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task12
+{
+    public class TestColor1 : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+    }
+}
