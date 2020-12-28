5aa2ad3e0242cd571837c23628003659795f010b
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2020-12-28 16:21:31 +0300

    feat: create new project and compleated task14

---
+++ src/TeachMeSkills.DotNet.Task14/IRecodable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task14
+{
+    public interface IRecodable
+    {
+        public void Record();
+
+        public void Pause();
+
+        public void Stop();
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var player = new Player();
+            var userInp = Console.ReadLine();
+
+            if(userInp.Contains("StopRec"))
+            {
+                player.Stop(userInp);
+            }
+            else if (userInp.Contains("Stop"))
+            {
+                player.Stop();
+            }
+            else if (userInp.Contains("Play"))
+            {
+                player.Play();
+            }
+            else if (userInp.Contains("PauseRec"))
+            {
+                player.Pause(userInp);
+            }
+            else if (userInp.Contains("Pause"))
+            {
+                player.Pause();
+            }
+
+        }
+    }
+}
