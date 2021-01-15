17d31ca643db022bdbc5171a347bec8096eb8948
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2021-01-15 15:17:05 +0300

    feat: create new project and compleated task 18

---
+++ src/TeachMeSkills.DotNet.Task18/SortArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task18
+{
+    public static class SortArray
+    {
+        public static int[] SortArrays(this int[] arrayNum)
+        {
+            return arrayNum.OrderBy(x => x).ToArray();
+        }
+    }
+}
