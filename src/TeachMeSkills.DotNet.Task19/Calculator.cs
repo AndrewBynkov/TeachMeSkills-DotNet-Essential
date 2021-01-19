898d939ff1690b8a34f498fef9be82460ca872ac
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2021-01-19 14:25:41 +0300

    feat: create new project and compleat Task19

---
+++ src/TeachMeSkills.DotNet.Task19/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task19
+{
+    public static class Calculator
+    {
+        public static decimal Plus(this decimal num1, decimal num2) => num1 + num2;
+        public static decimal Minus(this decimal num1, decimal num2) => num1 - num2;
+        public static decimal Devided(this decimal num1, decimal num2) => num1 / num2;
+        public static decimal Multiplicate(this decimal num1, decimal num2) => num1 * num2;
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task19/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"plus: {Calculator.Plus(14,15)}");
+            Console.WriteLine($"minus: {Calculator.Minus(14,15)}");
+            Console.WriteLine($"devided: {Calculator.Devided(14,15)}");
+            Console.WriteLine($"Multiplicate: {Calculator.Multiplicate(14,15)}");
+        }
+    }
+}
