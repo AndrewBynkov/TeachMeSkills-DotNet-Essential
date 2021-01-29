df243e672436d467e9dc515e1d50955f68f36191
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2021-01-29 15:16:41 +0300

    feat: create new project and compleated Task 20

---
+++ src/TeachMeSkills.DotNet.Task20/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TeachMeSkills.DotNet.Task20
+{
+    class Program
+    {
+        public static string DestinationInp { get; set; }
+
+        public static int NumberTrainInp { get; set; }
+
+        public static DateTime DateStartInp { get; set; }
+
+        public static List<Train> Trains { get; set; } = new List<Train>();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                UserInput();
+                var train = new Train(DestinationInp, NumberTrainInp, DateStartInp);
+                Train.AddToList(train, Trains);
+            }
+            Train.GetInfo(Train.SortTrains(Trains).ToList());
+            GetTrainNumber();
+        }
+
+        public static void UserInput()
+        {
+            Console.WriteLine();
+            Console.Write("Destination: ");
+            DestinationInp = Console.ReadLine();
+
+            Console.Write("Number train: ");
+            NumberTrainInp = int.Parse(Console.ReadLine());
+
+            Console.Write("Data start: ");
+            DateStartInp = DateTime.Parse(Console.ReadLine());
+        }
+
+        public static void GetTrainNumber()
+        {
+            Console.WriteLine();
+            Console.Write("Train number: ");
+            int userInp = int.Parse(Console.ReadLine());
+            Train.SelectNumberTrain(Trains, userInp);
+
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task20/StructTrain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task20
+{
+    public struct Train
+    {
+        public Train(string destination, int numberTrain, DateTime dateTimeStart )
+        {
+            _destination = destination;
+            _numberTrain = numberTrain;
+            _dateTimeStart = dateTimeStart;
+        }
+
+        private string _destination;
+        private int _numberTrain;
+        private DateTime _dateTimeStart;
+
+        public static List<Train> AddToList(Train train, List<Train> trains )
+        {
+            trains.Add(new Train
+            {
+                _numberTrain = train._numberTrain,
+                _dateTimeStart = train._dateTimeStart,
+                _destination = train._destination
+            });
+            return trains;
+        }
+
+        public static void SelectNumberTrain(List<Train> trains, int userInpNumber)
+        {
+            foreach (var item in trains)
+            {
+                if (item._numberTrain == userInpNumber)
+                {
+                    List<Train> selectTrain = new List<Train> { item };
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    GetInfo(selectTrain);
+                    Console.ResetColor();
+                }
+                if (item._numberTrain != userInpNumber)
+                {
+                    Console.WriteLine("Select train is availeble");
+                }
+            }
+        }
+
+        public static IList<Train> SortTrains(List<Train> trains)
+        {
+            return trains.OrderBy(x => x._numberTrain).ToList();
+        }
+
+        public static void GetInfo(List<Train> trains)
+        {
+            foreach (var item in trains)
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"Destination: {item._destination}");
+                Console.WriteLine($"Number train: {item._numberTrain}");
+                Console.WriteLine($"Time start: {item._dateTimeStart}");
+            }
+        }
+    }
+}
