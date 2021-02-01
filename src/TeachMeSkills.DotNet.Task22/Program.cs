2d5adb44659e777aab48fc4dcf5a1b762f1316b2
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2021-02-01 11:51:18 +0300

    feat: create new project and compleated Task 22

---
+++ src/TeachMeSkills.DotNet.Task22/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notebook = new StructNotebook("BMW", "General Motors", 150000);
+            notebook.ShowInfo();
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task22/StructNotebook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task22
+{
+    public struct StructNotebook
+    {
+        public StructNotebook(string model, string produced, int price)
+        {
+            Model = model;
+            Produced = produced;
+            Price = price;
+        }
+
+        public string Model { get; set; }
+        public string Produced { get; set; }
+        public decimal Price { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Model - {Model}, Produced - {Produced}, Price - {Price}");
+        }
+    }
+}
