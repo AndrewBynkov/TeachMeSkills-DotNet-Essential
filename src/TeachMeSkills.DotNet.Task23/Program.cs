fca89713b14f4542ad1f860a5775df0b7ed0acb5
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2021-02-02 13:53:04 +0300

    feat: create new project and compleated Task23

---
+++ src/TeachMeSkills.DotNet.Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticColor.Print("Cyan", 3);
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task23/StaticColor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task23
+{
+    public enum ColorConsolePrint
+    {
+        Blue = 1,
+        Green = 2,
+        Cyan = 3,
+        Red = 4
+    }
+
+    public static class StaticColor
+    {
+        public static void Print(string stroka, int color)
+        {
+
+            if (Enum.IsDefined(typeof(ColorConsolePrint), color))
+            {
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ColorConsolePrint), stroka);
+                Console.WriteLine(Enum.Parse(typeof(ColorConsolePrint), stroka));
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+        }
+    }
+}
