7e3ebe761e0a97020e10846b80aef52e694d3c0a
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2021-02-04 11:44:22 +0300

    feat: create new project and compleated Task 25

---
+++ src/TeachMeSkills.DotNet.Task25/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter date of you bithday: ");
+            var dateBithday = DateTime.Parse(Console.ReadLine());
+
+            int day = dateBithday.Day;
+            int month = dateBithday.Month;
+            int year = DateTime.Now.Year;
+            var dateTimeNow = DateTime.Now.Date;
+
+            DateTime dateBithDayNowYear = new DateTime(year, month, day);
+            Console.WriteLine(dateBithDayNowYear.Subtract(dateTimeNow));
+        }
+    }
+}
