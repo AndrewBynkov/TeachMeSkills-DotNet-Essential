8b670d96ba0f9e8533c3db0fe8a682a900123ae1
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2021-02-04 15:53:50 +0300

    feat: create new project and compleated Task 26

---
+++ src/TeachMeSkills.DotNet.Task26/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task26
+{
+    class Program
+    {
+        delegate int Operation(int x, int y);
+
+        static void Main(string[] args)
+        {
+
+            Operation Add = (x, y) => x + y;
+            Operation Minus = (x, y) => x - y;
+            Operation Multiplicate = (x, y) => x * y;
+            
+            Operation Devided = (x, y) => x / y;
+
+            Console.WriteLine(Add(10, 20));
+            Console.WriteLine(Minus(10, 20));
+            Console.WriteLine(Multiplicate(10, 20));
+            Console.WriteLine(Devided(10, 20));
+
+            Console.Read();
+        }
+
+    }
+}
+
